8480a39a81e4a46267a31dcc226fcbca46f11845
Usuario <usuario@example.com>
2025-09-04 00:12:43 -0300

    background: Finalização do background metálico com efeitos de aço escovado

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://paulomoura.com',
+  integrations: [],
+  vite: {
+    optimizeDeps: {
+      include: ['three'],
+      exclude: ['@react-three/fiber']
+    },
+    ssr: {
+      noExternal: ['three']
+    }
+  }
+});
